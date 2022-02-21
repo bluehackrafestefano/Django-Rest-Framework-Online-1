639efb58b9d9dc490e33d6c7730970c793452707
rafe <rafe@clarusway.com>
2022-02-21 10:20:47 +0100

    added home view

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
-# from django.urls import path
+from django.urls import path
+from .views import home
 
-# urlpatterns = [
-#     path('', home, name='home'),
-#     path('article', article_api)
-# ]
+urlpatterns = [
+    path('', home, name='home'),
+    # path('article', article_api)
+]
