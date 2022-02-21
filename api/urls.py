cc894808b48f987ffe17738b99d365e1fb8bd6c1
rafe <rafe@clarusway.com>
2022-02-21 12:56:13 +0100

    changed detail view as class based

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, article_list, article_detail, ArticleAPIView
+from .views import home, article_list, article_detail, ArticleAPIView, ArticleDetailAPIView
 
 urlpatterns = [
     path('', home, name='home'),
-    # path('article/', article_list, name='article_list'),
+    # path('article/', article_list, name='article_list'),  # function based view
     path('article/', ArticleAPIView.as_view(), name='article_list'),
-    path('article_detail/<int:pk>/', article_detail, name='article_detail'),
+    # path('article_detail/<int:pk>/', article_detail, name='article_detail'),
+    path('article_detail/<int:id>/', ArticleDetailAPIView.as_view(), name='article_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -54,6 +54,34 @@ def article_list(request):
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
+class ArticleDetailAPIView(APIView):
+    
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+    
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+    
+    
 # @csrf_exempt
 @api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
